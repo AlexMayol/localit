514310b8f25b48dbf35b2dfc99a26e24737642bc
Alex <alexmayolc@gmail.com>
2020-04-06 23:01:58 +0200

    new folder structure

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import {localit} from './localit';
-
-let ls = new localit('sessionStorage');
-ls.clear();
-ls.set('a', [1,2,3])
-console.log(ls.get('a'))
-
-ls.set('b', [4,5,6])
-console.log(ls.get('b'))
