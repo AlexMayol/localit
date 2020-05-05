cc77445043b5e6be3934140dd5bbbda12bd4be1b
Alex <alexmayolc@gmail.com>
2020-05-05 21:05:41 +0200

    basic documentation added

--- tests/code.js
+++ tests/code.js
@@ -1,8 +1,18 @@
-import {Localit} from '../src/localit.js';
+import { Localit } from '../src/localit.js';
 
 const store = new Localit();
-store.setDomain('youtubers')
 
-// store.set('willy', 'willy es el puto amo tt', '1h')
+store.setDomain('mammals_count');
+store.set('dog', '21');
+store.set('cat', '13');
 
-console.log(store.get('willy'))
+store.setDomain('birds_count');
+store.set('eagle', '10');
+store.set('dove', '200');
+
+console.log(localStorage)
+
+store.clearDomain('mammals_count')
+console.log(localStorage)
+store.bust()
+console.log(localStorage)
