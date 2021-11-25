462795aaf66d9878aef65aa95fbdd198ff7d7285
Alex <alexmayolc@gmail.com>
2021-11-26 00:58:30 +0100

    added a test to check the generated file size

---
+++ tests/file_size.test.ts
@@ -0,0 +1,20 @@
+import "regenerator-runtime/runtime";
+import fs from "fs";
+import util from "util";
+import zlib from "zlib";
+const readFile = util.promisify(fs.readFile);
+const gzip = util.promisify(zlib.gzip);
+
+const getGzipSize = (filePath) =>
+  readFile(filePath)
+    .then(gzip)
+    .then((x) => x.length);
+
+const MAX_ALLOWED_SIZE = 1500;
+const LOCALIT_PATH = "./dist/localit.js";
+
+describe("Localit's final file size", () => {
+  test("The generated file doesn't exceed 1.5KB after being gzipped", async () => {
+    expect(await getGzipSize(LOCALIT_PATH)).toBeLessThan(MAX_ALLOWED_SIZE);
+  });
+});
